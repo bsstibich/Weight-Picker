842fbbb3e99fd9375cd1d086b6bc38a85a136ef4
bsstibich <bsstibich@gmail.com>
2020-11-28 19:29:58 -0600

    Add different phone resolutions
    -allows for more thorough gui testing
    -gui now reads well on all 4 resoltions

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ import kivy
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.core.window import Window
-Window.size = (360, 640)
+#Window.size = (414, 896) #xr scaled down
+Window.size = (360, 760) #s10+ scaled down
+#Window.size = (360, 844) #i12 scaled down
+#Window.size = (375, 667) #generic scaled down
+
+
+
+
 
 class WeightPicker(App):
 	def build(self):
