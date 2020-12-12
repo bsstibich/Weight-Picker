c33b481893c5a581f2fb018684989b82ed578396
bsstibich <bsstibich@gmail.com>
2020-12-12 14:57:05 -0600

    delete buildozer from git

--- .buildozer/android/platform/build-armeabi-v7a/dists/weightpicker__armeabi-v7a/obj/local/armeabi-v7a/objs-debug/SDL2_mixer/music_flac.o.d
+++
@@ -1,2 +0,0 @@
-/home/turtletart/Weight-Picker/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/SDL2_mixer/music_flac.o: \
-  /home/turtletart/Weight-Picker/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_mixer/music_flac.c
