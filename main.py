399d40613f3b5656f22d93c7765ffc0b0862fb4c
bsstibich <bsstibich@gmail.com>
2020-12-12 15:07:48 -0600

    error handling for empty text box

--- main.py
+++ main.py
@@ -35,42 +35,46 @@ class FloatLayout(FloatLayout):
 			pickle.dump(self.bar, file)
 
 	def DoTheThing(self):
-		weight = int(self.weight.text) - self.bar
-		plates = {"45":0, "35":0, "25":0, "10":0, "5":0,"2.5":0}
-		output = "Weights On Each Side:\n\n"
-		if weight%10 == 5 or weight%10 == 0:
-			while weight > 0: 
-				if weight >= 90:
-					plates["45"] += 1
-					weight -= 90
-					pass
-				elif weight >= 70:
-					plates["35"] += 1
-					weight -= 70
-					pass
-				elif weight >= 50:
-					plates["25"] += 1
-					weight -= 50
-					pass
-				elif weight >= 20:
-					plates["10"] += 1
-					weight -= 20
-					pass
-				elif weight >= 10:
-					plates["5"] += 1
-					weight -= 10
-					pass
-				elif weight >=5:
-					plates["2.5"] += 1
-					weight -= 5
-					pass
-		else:
-			output = "Incorrect Input"
+		try:
+			weight = int(self.weight.text) - self.bar
+			plates = {"45":0, "35":0, "25":0, "10":0, "5":0,"2.5":0}
+			output = "Weights On Each Side:\n\n"
+			if weight%10 == 5 or weight%10 == 0:
+				while weight > 0: 
+					if weight >= 90:
+						plates["45"] += 1
+						weight -= 90
+						pass
+					elif weight >= 70:
+						plates["35"] += 1
+						weight -= 70
+						pass
+					elif weight >= 50:
+						plates["25"] += 1
+						weight -= 50
+						pass
+					elif weight >= 20:
+						plates["10"] += 1
+						weight -= 20
+						pass
+					elif weight >= 10:
+						plates["5"] += 1
+						weight -= 10
+						pass
+					elif weight >=5:
+						plates["2.5"] += 1
+						weight -= 5
+						pass
+			else:
+				output = "Incorrect Input"
+			
+			for plate in plates:
+				if plates[plate] > 0:
+					output += f"{plates[plate]}x{plate} lbs\n\n"
+			self.output.text = output
+		except:
+			print("Invalid Input")
 		
-		for plate in plates:
-			if plates[plate] > 0:
-				output += f"{plates[plate]}x{plate} lbs\n\n"
-		self.output.text = output
 
 class WeightPicker(App):
 
